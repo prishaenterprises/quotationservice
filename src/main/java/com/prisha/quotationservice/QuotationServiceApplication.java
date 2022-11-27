70e75f783c1d12e167e12e935bf7b75a96c35d8e
prishaenterprises <prishaenterprises0912@gmail.com>
2022-11-27 12:58:11 +0530

    Initial Commit

---
+++ src/main/java/com/prisha/quotationservice/QuotationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.prisha.quotationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuotationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuotationServiceApplication.class, args);
+	}
+
+}
